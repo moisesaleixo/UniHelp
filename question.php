986c9a8f00e4362e4ad850a5e555fc13b15375c2
moisesaleixo <moisesaleixoneto48@gmail.com>
2022-09-30 22:30:43 -0300

    Organizando o cabeçalho das páginas e logout

--- question.php
+++ question.php
@@ -9,10 +9,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Questionário</title>
-    <link rel="stylesheet" href="/css/quest.css">
+    <link rel="stylesheet" href="css/quest.css">
 </head>
 <body>
 
+    <main>
+
+        <header>
+            <div class="logo">
+                <a href="question.php"><img src="pictures/logo2.png" alt=""></a>
+            </div>
+            <div class="hamburger_menu">
+                <nav class="sidebar_menu">
+                    <ul>
+                        <li class="link"><a class="dados" href="perfil.php">Perfil</a></li>
+                        <li class="link"><a class="dados" href="config.php">Configurações</a></li>
+                        <li class="link"><a class="dados" href="logout.php">Sair</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <div>
+            <input class="foto_user" type="file" accept="image/*">
+        </div>
+
+    </main>
+
+
+
+<!--
     <main>
         <header>
             <div class="logoimg">
@@ -37,6 +63,6 @@
 
         </footer>
     </main>
-    
+-->
 </body>
 </html>
