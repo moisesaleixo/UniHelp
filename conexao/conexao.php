293833218b7d639189e8404a3d9e4ec4c50d1177
moisesaleixo <moisesaleixoneto48@gmail.com>
2022-11-16 12:18:51 -0300

    Conexão

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,8 +1,13 @@
 <?php
-
+    
+    $host = 'localhost';
+    $user = 'u225668871_user';
+    $senha = '6523338yMoises.';
+    $db_name = 'u225668871_unihelp_bd';
+    
     # Conectando ao host local, pedindo o usuário e a senha
-    $conexao = mysqli_connect('localhost', 'root', 'root');
+    $conexao = mysqli_connect($host, $user, $senha);
 
     # Selecionando o banco de dados e retornando erro se algo não funcionar
-    $banco = mysqli_select_db($conexao, 'unihelp') or die("Erro ao selecionar o banco de dados");
+    $banco = mysqli_select_db($conexao, $db_name) or die("Erro ao selecionar o banco de dados");
 ?>
